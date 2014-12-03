5b994f1ac7dcb008613ed461aec1613b4cb23b39
Akshay Bharath <akshay@animoto.com>
2014-12-03 16:33:22 -0500

    Initial Commit

---
+++ SCloudRandomizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SCloudRandomizer
+//
+//  Created by Akshay Bharath on 12/3/14.
+//  Copyright (c) 2014 Akshay Bharath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
