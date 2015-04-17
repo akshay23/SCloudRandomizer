77dd9a0e1f10302340faf12303fff18fb8971607
ashab <asha.ab@gmail.com>
2015-04-17 15:47:50 -0400

    Updated design for Info view
    1. Used a combination of UILabel and UITextView
    2. Removed the container view and scroll view
    3. Used Damascus font style
    4. Added images to describe tags and number of plays

--- SCloudRandomizer/TrackInfoVC.m
+++ SCloudRandomizer/TrackInfoVC.m
@@ -17,11 +17,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Draw border around both buttons
-    self.btnClose.layer.cornerRadius = 4;
-    self.btnClose.layer.borderWidth = 1;
-    self.btnClose.layer.borderColor = [UIColor blueColor].CGColor;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,17 +42,18 @@
         desc = @"No description";
     }
 
-    [self.txtTitle scrollRangeToVisible:NSMakeRange(0, 0)];
-    [self.txtTitle setText:[track objectForKey:@"title"]];
-    [self.txtArtist setText:[[track objectForKey:@"user"] objectForKey:@"username"]];
-    [self.txtDuration setText:[self convertFromMilliseconds:duration]];
-    [self.txtLikes setText:[[NSNumber numberWithLong:likes] stringValue]];
-    [self.txtPlays setText:[[NSNumber numberWithLong:plays] stringValue]];
-    [self.txtDesc scrollRangeToVisible:NSMakeRange(0, 0)];
-    [self.txtDesc setText:desc];
-    [self.txtUploaded setText:[uploaded substringToIndex:10]];
-    [self.txtTags scrollRangeToVisible:NSMakeRange(0, 0)];
-    [self.txtTags setText:[track objectForKey:@"tag_list"]];
+    [self.songTitle setText:[track objectForKey:@"title"]];
+    [self.songTitle sizeToFit];
+    
+    [self.artist setText:[[track objectForKey:@"user"] objectForKey:@"username"]];
+    [self.likes setText: [[NSNumber numberWithLong:likes] stringValue]];
+    [self.numOfPlays setText: [[NSNumber numberWithLong:plays] stringValue]];
+    [self.songDescription setText:desc];
+    
+    [self.uploadedTime
+      setText: [NSString stringWithFormat: @"Uploaded at %@", [uploaded substringToIndex:10]]];
+    
+    [self.tags setText:[track objectForKey:@"tag_list"]];
     
     NSLog(@"Track details displayed!");
 }
@@ -72,23 +68,6 @@
     
     [self.view.window.layer addAnimation:animation forKey:kCATransition];
     [self dismissViewControllerAnimated:NO completion:nil];
-}
-
-// Convert to ms
-- (NSString *)convertFromMilliseconds:(long)duration
-{
-    NSInteger minutes = floor(duration / 60000);
-    NSInteger seconds = ((duration % 60000) / 1000);
-    NSString *formatted = nil;
-    if (seconds < 10)
-    {
-        formatted = [NSString stringWithFormat:@"%ld:0%ld", (long)minutes, (long)seconds];
-    }
-    else
-    {
-        formatted = [NSString stringWithFormat:@"%ld:%ld", (long)minutes, (long)seconds];
-    }
-    return  formatted;
 }
 
 @end
